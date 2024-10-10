628a3ac1d623f04192bec76e0b5e101b463fcab1
janjao937 <janjao937@gmail.com>
2024-10-11 02:06:52 +0700

    docker compose

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS patientsdb;
 USE patientsdb;
-DROP TABLE IF EXISTS patientsdb;
+DROP TABLE IF EXISTS patients;
 
 CREATE TABLE patients
 (
@@ -8,6 +8,7 @@ CREATE TABLE patients
     first_name VARCHAR(255) DEFAULT NULL,
     last_name VARCHAR(255) DEFAULT NULL,
     address VARCHAR(255) DEFAULT NULL,
+    email VARCHAR(255) DEFAULT NULL,
     diagnosis VARCHAR(255) DEFAULT NULL,
     phone VARCHAR(30) DEFAULT NULL,
     status VARCHAR(30) DEFAULT NULL,
