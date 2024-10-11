9064ba762bb5b33a99572ea6353d4038f45c1c60
janjao937 <janjao937@gmail.com>
2024-10-11 19:04:23 +0700

    fixed mysql compose can create database *manual create table

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS patientsdb;
 USE patientsdb;
 DROP TABLE IF EXISTS patients;
 
-CREATE TABLE patients
+CREATE TABLE patients 
 (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255) DEFAULT NULL,
@@ -11,10 +11,10 @@ CREATE TABLE patients
     email VARCHAR(255) DEFAULT NULL,
     diagnosis VARCHAR(255) DEFAULT NULL,
     phone VARCHAR(30) DEFAULT NULL,
-    status VARCHAR(30) DEFAULT NULL,
+    status VARCHAR(30) DEFAULT 'Pending',
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     image_url VARCHAR(255) DEFAULT NULL,
 
     PRIMARY KEY (id),
-    CONSTRAINT UQ_patients_Email UNIQUE (email)
+    CONSTRAINT UQ_Users_Email UNIQUE (email)
 )AUTO_INCREMENT=1;
